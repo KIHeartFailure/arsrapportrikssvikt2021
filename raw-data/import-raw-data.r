085338f8e896050742c7b54b73ccc8a6315898ac
Lina Benson <lina.benson@ki.se>
2022-04-04 10:45:24 +0200

    ändra till incident tg 2020

--- raw-data/import-raw-data.r
+++ raw-data/import-raw-data.r
@@ -25,14 +25,14 @@ save(file = "./data/rawData_rs.RData", list = c(
 prevtime <- read.xlsx("./raw-data/tg/2617_2022 RiksSvikt - TG Prev mot Prev - Huvuddiagnos 2010-2021 2022-04-01_klar_LB.xlsx",
   sheet = "Län per år"
 )
-inctime <- read.xlsx("./raw-data/tg/2617_2022 RiksSvikt - TG Inc mot Inc 2003-2021 - Bryt vid årsskifte 2022-04-01 Lev2_klar_LB.xlsx",
+inctime <- read.xlsx("./raw-data/tg/2617_2022 RiksSvikt - TG Inc mot Inc 2003-2020 - Registrering inom 1 år 2022-04-01 Lev2_klar_LB.xlsx",
   sheet = "Län per år"
 )
 prev <- read.xlsx("./raw-data/tg/2617_2022 RiksSvikt - TG Prev mot Prev - Huvuddiagnos 2010-2021 2022-04-01_klar_LB.xlsx",
   sheet = "2021"
 )
-inc <- read.xlsx("./raw-data/tg/2617_2022 RiksSvikt - TG Inc mot Inc 2003-2021 - Bryt vid årsskifte 2022-04-01 Lev2_klar_LB.xlsx",
-  sheet = "2021"
+inc <- read.xlsx("./raw-data/tg/2617_2022 RiksSvikt - TG Inc mot Inc 2003-2020 - Registrering inom 1 år 2022-04-01 Lev2_klar_LB.xlsx",
+  sheet = "2020"
 )
 
 prev <- prev %>% as_tibble(.name_repair = "unique")
